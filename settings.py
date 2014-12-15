60bafecc085269a3d012bfe30483e366189e0b3e
sunqi0928 <sunqi0928@gmail.com>
2014-12-15 05:56:19 +0000

    10.21.2.97

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ LISTEN_PORT = 8088
 
 COUCHBASE_BUCKET = os.getenv("COUCHBASE_BUCKET", "default")
 COUCHBASE_HOST = os.getenv("COUCHBASE_HOST",
-    os.getenv("COUCHBASE_PORT_8091_TCP_ADDR", "10.21.1.232"))
+    os.getenv("COUCHBASE_PORT_8091_TCP_ADDR", "10.21.2.97"))
 
 ASSET_BUNDLE_ROOT = os.getenv("ASSET_BUNDLE_ROOT", "/opt/assetbundle")  # must be absolute path
